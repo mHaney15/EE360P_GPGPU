4b2424a3cc8a3845ff82d3720b0a7f5c2eff83bb
mHaney15 <matthew.haney@utexas.edu>
2016-04-23 19:40:32 -0500

    working vector yo

--- GPGPU_Comparisons/src/GPGPU_Comparisons/GPGPGU_Driver.java
+++ GPGPU_Comparisons/src/GPGPU_Comparisons/GPGPGU_Driver.java
@@ -26,7 +26,7 @@ import org.lwjgl.opencl.CLPlatform;
 import static org.lwjgl.opencl.CL10.*;
 
 public class GPGPGU_Driver {
-	public static final int SIZE = 32;
+	public static final int SIZE = 64;
 	
 	public static CLContext context;
 	public static CLPlatform platform;
@@ -40,16 +40,15 @@ public class GPGPGU_Driver {
 	
 	public static void main(String[] args){
 		try {
+			
+			long Begin_Full = System.nanoTime();
 			//Read in Kernel file.
 			kernelString = readFile("src/Vec_Add.cl");
-			System.out.println(kernelString);
+			//System.out.println(kernelString);
 			initializeCL();
 			//Need to create matrices/vectors here!
 			double[] A = RMVG.getRandomVector(SIZE);
 			double[] B = RMVG.getRandomVector(SIZE);
-			double[] CPU_Result = CPUProcessing.vectorAddition(A, B);
-			System.out.println("CPU Result:");
-			System.out.println(Arrays.toString(CPU_Result));
 			a = toDoubleBuffer(A);
 			b = toDoubleBuffer(B);
 			answer = BufferUtils.createDoubleBuffer(a.capacity());
@@ -77,23 +76,18 @@ public class GPGPGU_Driver {
 	        // Execution our kernel
 	        PointerBuffer kernel1DGlobalWorkSize = BufferUtils.createPointerBuffer(1);
 	        kernel1DGlobalWorkSize.put(0, a.capacity());
+	        long Begin_gpuComp = System.nanoTime();
 	        kernel.setArg(0, aMem);
 	        kernel.setArg(1, bMem);
 	        kernel.setArg(2, answerMem);
 	        kernel.setArg(3, nMem);
 	        clEnqueueNDRangeKernel(queue, kernel, 1, null, kernel1DGlobalWorkSize, null, null, null);
-	        
-	     // Read the results memory back into our result buffer
+	        long End_gpuComp = System.nanoTime();
+	        // Read the results memory back into our result buffer
 	        clEnqueueReadBuffer(queue, answerMem, 1, 0, answer, null, null);
 	        clFinish(queue);
-	        
-	        printVector(answer);
 	        double[] GPU_Result = RMVG.BufferToArray(answer);
-	        if(RMVG.CompareResults(GPU_Result, CPU_Result)){
-	        	System.out.println("They Matched!");
-	        }
-	        
-	        // Clean up OpenCL resources
+	     // Clean up OpenCL resources
 	        clReleaseKernel(kernel);
 	        clReleaseProgram(program);
 	        clReleaseMemObject(aMem);
@@ -103,6 +97,26 @@ public class GPGPGU_Driver {
 	        clReleaseCommandQueue(queue);
 	        clReleaseContext(context);
 	        CL.destroy();     
+	        long End_Full = System.nanoTime();
+	        
+	        long Begin_cpuComp = System.nanoTime();
+	        double[] CPU_Result = CPUProcessing.vectorAddition(A, B);
+	        long End_cpuComp = System.nanoTime();
+	        
+	        System.out.println("GPU Results:");
+	        System.out.println(Arrays.toString(GPU_Result));
+	        System.out.println("\nCPU_Results:");
+	        System.out.println(Arrays.toString(CPU_Result));
+	        if(RMVG.CompareResults(GPU_Result, CPU_Result)){
+	        	System.out.println("They Matched!\n");
+	        }
+	        
+	        System.out.println("\tTIME COMPARISONS:");
+	        System.out.println("GPU,FULL:\t" + (End_Full - Begin_Full));
+	        System.out.println("GPU,EXEC:\t" + (End_gpuComp - Begin_gpuComp));
+	        System.out.println("CPU,EXEC:\t" + (End_cpuComp - Begin_cpuComp));
+	        
+	        
 	        
 		} catch (LWJGLException e) {
 			System.err.println("Looks like your system does not support OpenCL. :(");
